1e91a647c3f6f9ac5dc7d167c944f37acbdafc70
hikuley <halil.ibrahimkuley@gmail.com>
2021-09-24 11:27:51 +0100

    Fix start/stop scripts

--- shutdown.sh
+++ shutdown.sh
@@ -9,3 +9,24 @@ function exitIfNonZeroStatus() {
 
 echo "Servers are stopping..."
 kill -9 $(cat ./_dist/pid-api-gateway.file) $(cat ./_dist/pid-user-microservice.file)
+
+
+api_gateway_pid_file="_dist/pid-api-gateway.file"
+if [ -f "$api_gateway_pid_file" ] ; then
+    rm "$api_gateway_pid_file"
+fi
+
+user_micro_service_pid_file="_dist/pid-user-microservice.file"
+if [ -f "$user_micro_service_pid_file" ] ; then
+    rm "$user_micro_service_pid_file"
+fi
+
+api_gateway_log_file="_dist/api-gateway.log"
+if [ -f "$api_gateway_log_file" ] ; then
+    rm "$api_gateway_log_file"
+fi
+
+user_micro_service_log_file="_dist/user-microservice.log"
+if [ -f "$user_micro_service_log_file" ] ; then
+    rm "$user_micro_service_log_file"
+fi
--- startup.sh
+++ startup.sh
@@ -7,14 +7,34 @@ function exitIfNonZeroStatus() {
   fi
 }
 
+api_gateway_pid_file="_dist/pid-api-gateway.file"
+if [ -f "$api_gateway_pid_file" ] ; then
+    rm "$api_gateway_pid_file"
+fi
+
+user_micro_service_pid_file="_dist/pid-user-microservice.file"
+if [ -f "$user_micro_service_pid_file" ] ; then
+    rm "$user_micro_service_pid_file"
+fi
+
+api_gateway_log_file="_dist/api-gateway.log"
+if [ -f "$api_gateway_log_file" ] ; then
+    rm "$api_gateway_log_file"
+fi
+
+user_micro_service_log_file="_dist/user-microservice.log"
+if [ -f "$user_micro_service_log_file" ] ; then
+    rm "$user_micro_service_log_file"
+fi
+
 echo "api-gateway is starting..."
-nohup java -jar ./_dist/api-gateway-0.0.1-SNAPSHOT.jar > ./_dist/api-gateway.log 2>&1 &
-echo $! > ./_dist/pid-api-gateway.file
+nohup java -jar ./_dist/api-gateway-0.0.1-SNAPSHOT.jar > $api_gateway_log_file 2>&1 &
+echo $! > $api_gateway_pid_file
 
 exitIfNonZeroStatus
 
 echo "user-microservice is starting..."
-nohup java -jar ./_dist/user-microservice-0.0.1-SNAPSHOT.jar > ./_dist/user-microservice.log 2>&1 &
-echo $! > ./_dist/pid-user-microservice.file
+nohup java -jar ./_dist/user-microservice-0.0.1-SNAPSHOT.jar > $user_micro_service_log_file 2>&1 &
+echo $! > $user_micro_service_pid_file
 
 exitIfNonZeroStatus
